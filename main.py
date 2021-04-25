651d564cbebd5edc440554363cb4cb71b4b9a6e4
directx5 <73019195+directx5@users.noreply.github.com>
2021-04-25 11:58:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import steam.enums.common
 with open("config", "r") as f:
     config = f.read().split("\n")
     account = config[0].split(":")
-    games = list(map(int, [x.strip() for x in set(config[1].split(",")) if x.strip() != "" and x.strip().isdigit()]))
+    games = list(map(int, [x.strip() for x in set(config[1].split(",")) if x.strip().isdigit()]))
 
 os.system("title Steam Hour Booster")
 client = steam.client.SteamClient()
