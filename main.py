35728694246e49d7fd31ebee47fa9e499acc1097
directx5 <73019195+directx5@users.noreply.github.com>
2021-05-16 22:42:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import os
 import ctypes
 import datetime
+import os
 import subprocess
 
 import steam.client
 
 if not os.path.exists("config"):
     with open("config", "w", encoding="UTF-8") as config:
-        config.write("username:password\ngameid,gameid,gameid,gameid,gameid,gameid")
+        config.write(f"username:password\n{','.join(['game_id'] * 10)}")
     subprocess.call(["notepad", "config"])
 else:
     if str(input("Do you want to open the config file? [Y/N]: ")).lower() == "y":
@@ -15,13 +15,12 @@ else:
 
 with open("config", "r", encoding="UTF-8") as f:
     config = f.read().split("\n")
-    account = config[0].split(":")
-    games = [int(x.strip()) for x in set(config[1].split(",")) if x.strip().isdigit()]
+    account = [x.strip() for x in config[0].split(":")[:2]]
+    games = [int(c) for x in set(config[1].split(",")) if (c := x.strip()).isdigit()]
 
 os.system("title Steam Hour Booster")
 client = steam.client.SteamClient()
 client.cli_login(*account)
-client.change_status(persona_state=1)
 client.games_played(games)
 os.system("cls")
 
@@ -31,7 +30,6 @@ while True:
         break
     else:
         current_time = str(datetime.datetime.now() - start).split(".")[0]
-        os.system(f"title Steam Hour Booster - {client.user.name} - {current_time}")
         print(f"\r[Steam Hour Booster] -> Username: [{client.user.name}] | Boosting For: [{current_time}]", end="")
 
 client.logout()
